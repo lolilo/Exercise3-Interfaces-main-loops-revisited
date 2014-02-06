8812b7ca7f3f2bc538d08f49905c86110fe6da3f
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 16:06:42 -0800

    fixed empty strings

--- ex3.py
+++ ex3.py
@@ -4,23 +4,35 @@ operators = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def validate_input(user_input):
+    print user_input
     for i in user_input:
+        #print "Evaluating %s" % i
         is_a_number = True
         for char in i:
             if char not in numbers:
+                #print char + " is not a number."
                 is_a_number = False
         if is_a_number == False and i not in operators:
             print "To use this calculator, please input an operator followed by numbers."
-            break
+            return False
 
 
     if user_input[0] not in operators:
         print "Please use one of the following operators (or q for quit): "
         print operators
         return False
-    #elif len(user_input) > 3 or len(user_input) < 1:
-     #   print ""
+
     return True
+
+def how_many_numbers(user_input):
+    return len(user_input)
+
+def convert_nums_to_int(nums):
+    converted_nums = []
+    for n in nums:
+        converted_nums.append(int(n))
+     #   print converted_nums
+    return converted_nums
 
 while True:
     raw_numbers = raw_input("> ")
@@ -30,31 +42,56 @@ while True:
 
     tokens = raw_numbers.split(" ")
 
-    operator = tokens[0]
-
-    nums = tokens[1:]
+    for i in range(tokens.count('')):
+        tokens.remove('')
 
     if validate_input(tokens) == False:
         continue
 
-    n1 = int(nums[0])
+    operator = tokens[0]
+    #print "tokens",tokens[1:]
 
-    if len(nums) > 1:
-        n2 = int(nums[1])
+    nums = convert_nums_to_int(tokens[1:])
+
+    num_args = how_many_numbers(nums)
+
+    error_string = "For %s, pleae input \" %r \" followed by %s numbers, separated by spaces." 
 
     if operator == '+':
-        print art.add(n1, n2)
+        if num_args != 2:
+            print error_string %("addition", "+", "two")
+        else: 
+            print art.add(nums[0], nums[1])
+
     elif operator == '-':
-        print art.subtract(n1, n2)
+        if num_args != 2:
+            print error_string %("subtraction", "-", "two")
+        else:
+            print art.subtract(nums[0], nums[1])
+
     elif operator == '*':
-        print art.multiply(n1, n2)
+        if num_args != 2:
+            print error_string %("multiplication", "*", "two")
+        else: 
+            print art.multiply(nums[0], nums[1])
+
     elif operator == '/':
-        print art.divide(n1, n2)
+        if num_args != 2:
+            print error_string %("division", "/", "two")
+        else: 
+            print art.divide(nums[0], nums[1])
+
     elif operator == 'square':
-        print art.square(n1)
+        if num_args != 1:
+            print error_string %("square", "square", "one")
+        else: 
+            print art.square(nums[0])
+
     elif operator == 'cube':
-        print art.cube(n1)
+        print art.cube(nums[0])
+
     elif operator == 'pow':
-        print art.power(n1, n2)
+        print art.power(nums[0], nums[1])
+
     elif operator == 'mod':
-        print art.mod(n1,n2)
+        print art.mod(nums[0],nums[1])
